b1fcdda9bbeafa8165f8a84c22c590cc3db4a9ee
wil-mus <wilfred_musyoka@yahoo.com>
2023-05-13 13:49:57 -0700

    gaurd header files

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <string.h>
+
+/**  FUNCTIONS PROTOTYPES */
+void print_rev(char *s);
+
+#endif
