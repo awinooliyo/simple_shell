38ba6394c6240bd5e8b3faac84eceef2fe43eeb2
Erick Awino <erick.otieno89@gmail.com>
2023-05-13 22:47:15 +0300

    Task number 0 main file

---
+++ main_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+tr = “A clown on a throne istill a clown — C. JoyBell C";
+	print_rev(str);
+	return (0);
+}
