2f9a93a8df4425667f83cbf4fc4c1c805df66db5
wil-mus <wilfred_musyoka@yahoo.com>
2023-05-13 13:49:30 -0700

    printing string in reverse task 0

---
+++ print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - A function that prints strings in reverse
+ *
+ * @s:  pointer to the string
+ */
+
+void print_rev(char *s)
+{
+	int a, len;
+
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (a = len -1; a >= 0; a--)
+	{
+		printf("%c", s[a]);
+	}
+	printf("\n");
+}
