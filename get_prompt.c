cd22757cfbaa8567485e80fd8574386d15b2c618
wil-mus <wilfred_musyoka@yahoo.com>
2023-05-14 06:10:18 -0700

    add a new element to make it interactive

--- get_prompt.c
+++ get_prompt.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
+ * get_prompt - A function that displays a prompt
+ *
+ * @av: argument vector
+ * @env: variable environment
+ *
  */
 void get_prompt(char **av, char **env)
 {
 	char *str = NULL;
 	size_t n = 0;
 	ssize_t read;
-	char *argv[] = {NULL};
+	char *argv[2];
 	pid_t pid;
 	int status;
 
@@ -25,6 +30,7 @@ void get_prompt(char **av, char **env)
 		str[strcspn(str, "\n")] = '\0';
 		
 		argv[0] = str;
+		argv[1] = NULL;
 		pid = fork();
 		if (pid == -1)
 		{
