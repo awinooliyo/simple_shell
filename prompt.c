30291a910883eb93cb9c0a139d5ca1d760474fa5
wil-mus <wilfred_musyoka@yahoo.com>
2023-05-16 10:54:25 -0700

    l

--- prompt.c
+++ prompt.c
@@ -3,13 +3,17 @@
 /**
  * main - check code
  *
+ * @ac: argument count
+ * @av: argument vector
+ * @env: environment variables
+ *
  * Return: 0
  */
 int main(int ac, char **av, char **env)
 {
-        if (ac == 1)
-        {
-                get_prompt(av, env);
-        }
-        return (0);
+	if (ac == 1)
+	{
+		get_prompt(av, env);
+	}
+	return (0);
 }
