eac46d82301c39ac9ebe49f03b502d2732c75dbb
Erick Awino <erick.otieno89@gmail.com>
2023-05-22 09:15:25 +0300

    Adding the Alias commands

--- getline.c
+++ getline.c
@@ -3,20 +3,20 @@
 /**
  * _getline - A custom getline function that reads characters into a buffer
  *
- * @str: pointer to the input string
- * @n: pointer to the size of the input buffer
+ * @str: pointer to the inputtring
+ * @n: pointer to theize of the input buffer
  *
  * Return: no of characters read (excluding newline), or -1 on failure
  */
 
-ssize_t _getline(char **str, size_t *n)
+ssize_t _getline(char **str,ize_t *n)
 {
-	static char buf[BUFFER_SIZE];
-	static size_t p = 0;
-	static ssize_t size = 0;
-	ssize_t a;
+tatic char buf[BUFFER_SIZE];
+taticize_t p = 0;
+taticsize_tize = 0;
+size_t a;
 	char *temp;
-	ssize_t bytesRead;
+size_t bytesRead;
 
 	if (p >= (size_t)size)
 	{
@@ -28,12 +28,12 @@ ssize_t _getline(char **str, size_t *n)
 			return (0);
         	p = 0;
 
-		size = bytesRead;
+ize = bytesRead;
 	}
 
 	a = p;
 
-	while (a < size && buf[a] != '\n')
+	while (a <ize && buf[a] != '\n')
 		a++;
 
 	if (a - p > *n)
@@ -62,8 +62,8 @@ ssize_t _getline(char **str, size_t *n)
 int m_getline(void)
 {
 	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
+ize_t len = 0;
+size_t nread;
 
 	while ((nread = _getline(&line, &len)) != -1)
 	{
